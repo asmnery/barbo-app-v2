0336211e6aa19f75e7317bdd010ad1da172db348
Alberth Nery <iophyhd@gmail.com>
2024-10-26 00:11:39 -0300

    feat: add styles, backgrounds, fonts in the pages, etc

--- static/script.js
+++ static/script.js
@@ -1,24 +1,17 @@
-// Exibir mensagens de erro ou sucesso
-document.addEventListener("DOMContentLoaded", function() {
-  const flashMessage = document.querySelector('.flash');
-  if (flashMessage) {
-      setTimeout(() => {
-          flashMessage.style.display = 'none';
-      }, 3000); // Oculta a mensagem após 3 segundos
-  }
-
-  // Validação simples para o formulário de agendamentos
-  const agendamentosForm = document.getElementById('agendamentosForm');
-  if (agendamentosForm) {
-      agendamentosForm.addEventListener('submit', function(event) {
-          const cliente = document.getElementById('cliente').value;
-          const servico = document.getElementById('servico').value;
-          const data = document.getElementById('data').value;
-
-          if (!cliente || !servico || !data) {
-              event.preventDefault();
-              alert('Por favor, preencha todos os campos do formulário!');
-          }
-      });
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    const dataInput = document.getElementById('data');
+    
+    // Função para formatar a data no formato desejado
+    function formatDate() {
+        const date = new Date(dataInput.value);
+        if (!isNaN(date)) {
+            // Formato dd/mm/yyyy
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            dataInput.value = `${day}/${month}/${year}`;
+        }
+    }
+    
+    dataInput.addEventListener('change', formatDate);
 });
